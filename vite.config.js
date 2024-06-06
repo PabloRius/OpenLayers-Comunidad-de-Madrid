a649766ea141e6e2b6e15ef4506f2301efc583c3
PabloRius <pablogrius@gmail.com>
2024-06-06 18:24:09 +0200

    Change vite build config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.geojson"],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom"],
+          "openlayers-vendor": ["ol"],
+        },
+      },
+    },
+  },
 });
